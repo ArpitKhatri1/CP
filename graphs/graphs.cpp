b1e636ed40572ffa86854a27e65d4f422f68c2eb
ArpitKhatri1 <arpitkumarkhatri@gmail.com>
2025-02-07 22:46:13 +0530

    dfs

--- graphs/graphs.cpp
+++ graphs/graphs.cpp
@@ -4,13 +4,16 @@ vector<vector<int>> adj;
 
 void dfs(int vertex)
 {
-
     // take action on vertex after entering the vertex
 
     for (int child : adj[vertex]) // this loop cant move forwared till we can cover the depth of the child
     {
+        // take action on child before entering the child node
         dfs(child);
+        // take action on child after exiting the child node
+        // after returning after complete dfs of its tree
     }
+    // take action on vertex after exiting the vertex
 }
 
 int main()
