690bcf5550f9ffba6d1b7de740ff8d2f30188b3d
ArpitKhatri1 <arpitkumarkhatri@gmail.com>
2025-02-12 19:19:10 +0530

    union-by-size-added

--- graphs/union-find.cpp
+++ graphs/union-find.cpp
@@ -3,12 +3,13 @@ using namespace std;
 
 class DisjointSet
 {
-    vector<int> rank, parent;
+    vector<int> rank, parent, size;
 
 public:
     DisjointSet(int n)
     {
         rank.resize(n + 1, 0);
+        size.resize(n + 1, 1);
         parent.resize(n + 1);
         for (int i = 0; i <= n; i++)
         {
@@ -47,16 +48,36 @@ public:
             rank[up]++;
         }
     }
+
+    void unionBySize(int u, int v)
+    {
+        int up = findParent(u);
+        int vp = findParent(v);
+        if (up == vp)
+        {
+            return;
+        }
+        if (size[up] < size[vp])
+        {
+            parent[up] = vp;
+            size[vp] += size[up];
+        }
+        else
+        {
+            parent[vp] = up;
+            size[up] += size[vp];
+        }
+    }
 };
 
 int main()
 {
     DisjointSet ds(7);
-    ds.unionByRank(1, 2);
-    ds.unionByRank(2, 3);
-    ds.unionByRank(4, 5);
-    ds.unionByRank(6, 7);
-    ds.unionByRank(5, 6);
+    ds.unionBySize(1, 2);
+    ds.unionBySize(2, 3);
+    ds.unionBySize(4, 5);
+    ds.unionBySize(6, 7);
+    ds.unionBySize(5, 6);
 
     if (ds.findParent(3) == ds.findParent(4))
     {
@@ -66,7 +87,7 @@ int main()
     {
         cout << "not same";
     }
-    ds.unionByRank(3, 7);
+    ds.unionBySize(3, 7);
 
     if (ds.findParent(3) == ds.findParent(4))
     {
