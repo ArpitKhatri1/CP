76c5120029e7d575cf9a03d359b61fe76de20151
ArpitKhatri1 <arpitkumarkhatri@gmail.com>
2025-02-12 22:16:33 +0530

    correct-code

--- graphs/union-find.cpp
+++ graphs/union-find.cpp
@@ -1,59 +1,76 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-class DisjointSet {
-  vector<int> rank, parent, size;
+class DisjointSet
+{
 
 public:
-  DisjointSet(int n) {
+  vector<int> rank, parent, size;
+  DisjointSet(int n)
+  {
     rank.resize(n + 1, 0);
     size.resize(n + 1, 1);
     parent.resize(n + 1);
-    for (int i = 0; i <= n; i++) {
+    for (int i = 0; i <= n; i++)
+    {
       parent[i] = i;
     }
   }
 
-  int findParent(int node) {
-    if (node == parent[node]) {
+  int findParent(int node)
+  {
+    if (node == parent[node])
+    {
       return node;
     }
     return parent[node] = findParent(parent[node]);
   }
 
-  void unionByRank(int u, int v) {
+  void unionByRank(int u, int v)
+  {
     int up = findParent(u);
     int vp = findParent(v);
-    if (up == vp) {
+    if (up == vp)
+    {
       return;
     }
-    if (rank[up] < rank[vp]) {
+    if (rank[up] < rank[vp])
+    {
       parent[up] = vp;
-    } else if (rank[vp] < rank[up]) {
+    }
+    else if (rank[vp] < rank[up])
+    {
       parent[vp] = up;
-    } else {
+    }
+    else
+    {
       parent[vp] = up;
       rank[up]++;
     }
   }
 
-  void unionBySize(int u, int v) {
+  void unionBySize(int u, int v)
+  {
     int up = findParent(u);
     int vp = findParent(v);
-    if (up == vp) {
+    if (up == vp)
+    {
       return;
     }
-    if (size[up] < size[vp]) {
+    if (size[up] < size[vp])
+    {
       parent[up] = vp;
       size[vp] += size[up];
-    } else {
+    }
+    else
+    {
       parent[vp] = up;
       size[up] += size[vp];
     }
   }
 };
 
-int main() {
+int main()
+{
   DisjointSet ds(7);
   ds.unionBySize(1, 2);
   ds.unionBySize(2, 3);
@@ -61,16 +78,22 @@ int main() {
   ds.unionBySize(6, 7);
   ds.unionBySize(5, 6);
 
-  if (ds.findParent(3) == ds.findParent(4)) {
+  if (ds.findParent(3) == ds.findParent(4))
+  {
     cout << "Same";
-  } else {
+  }
+  else
+  {
     cout << "not same";
   }
   ds.unionBySize(3, 7);
 
-  if (ds.findParent(3) == ds.findParent(4)) {
+  if (ds.findParent(3) == ds.findParent(4))
+  {
     cout << "Same";
-  } else {
+  }
+  else
+  {
     cout << "not same";
   }
 
